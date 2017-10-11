e0b8455e074883c7604b3715cfe7b638e30d4f91
tianke.lyq <tianke.lyq@alibaba-inc.com>
2017-10-11 17:40:44 +0800

    add config

--- server/config.js
+++ server/config.js
@@ -1,14 +1,14 @@
 config = {
   // 服务端端口，需要与disqus-proxy前端设置一致
-  port: 5509,
+  port: (process.env.PORT || 5000),
 
   // 你的diqus secret key
-  api_secret: 'your secret key',
+  api_secret: '7VNMmAGL2atqWiGKWIDzypl1x8s4XRCR2D4DMg8ZPB9i8vMKf1Fsr89aT5ROSVmb',
 
   // 你的website的 shortname 名称 比如在你的disqus安装代码中 有这样一句脚本：
   // s.src = 'https://test-eo9kkdlcze.disqus.com/embed.js';
   // 那么你的disqus 的shortname 就是 test-eo9kkdlcze
-  shortname: 'ciqu',
+  shortname: 'lewis617s-blog',
 
   // 服务端socks5代理转发，便于在本地测试，生产环境通常为null
   // socks5Proxy: {
