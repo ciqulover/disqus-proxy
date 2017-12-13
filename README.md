e0b180cd1b03213f61880b176ddd88e21dd690a4
ciqulover <ciqulover@gmail.com>
2017-12-13 18:15:37 +0800

    分离项目

--- README.md
+++ README.md
@@ -9,7 +9,8 @@
 
 ### 准备
 * 一台国外的VPS服务器
-* 一点点的HTML基本知识
+* 基本的命令行相关知识
+* 一点点hexo的使用经验
 
 ### 安装
 
@@ -35,7 +36,7 @@ disqus_proxy:
          那么你的disqus 的shortname 就是 test-eo9kkdlcze
 * `username` 是你的disqus用户名，即评论时候留下的名字，用来区别disqus-proxy的评论头像显示
 * `host`是你启用disqus代理的VPS的域名
-* `port`是VPS服务器启用disqus代理的端口，需要与之后设置的后端一致
+* `port`是VPS服务器启用disqus代理的端口，需要与之后配置的后端一致
 
 #### 关键的一步
 
@@ -56,99 +57,4 @@ disqus_proxy:
 
 ### 后端配置
 
-
-#### 获取`api-secret`
-
-在使用本插件之前需要在 Disqus 申请开启 api 权限。访问[register new application](https://disqus.com/api/applications/register/) 就可以注册一个 application.然后在[applications](https://disqus.com/api/applications/)可以看到你的 application 列表。其中 Secret Key 就是我们需要的api-secret,并且需要在后台的Settings => Community里开启访客评论
-
-后端使用`Node.js`，需要`Node.js`版本`7.6`以上。
-
-#### 在服务器上clone代码:
-```
-git clone https://github.com/ciqulover/disqus-proxy
-```
-#### 安装依赖 
-只需要安装后端的依赖
-```
-npm i --production
-// 或者
-yarn install --production
-```
-
-#### 配置`server`目录下的`config.js`
-``` js
-module.exports = {
-  // 服务端端口，需要与disqus-proxy前端设置一致
-    port: 5509,
-  
-    // 你的diqus secret key
-    api_secret: 'your secret key',
-  
-    // 你的website的 shortname 名称 比如在你的disqus安装代码中 有这样一句脚本：
-    // s.src = 'https://test-eo9kkdlcze.disqus.com/embed.js';
-    // 那么你的disqus 的shortname 就是 test-eo9kkdlcze
-    shortname: 'ciqu',
-  
-    // 服务端socks5代理转发，便于在本地测试，生产环境通常为null
-    // socks5Proxy: {
-    //   host: 'localhost',
-    //   port: 1086
-    // },
-  
-    socks5Proxy: null,
-  
-    // 日志输出位置,输出到文件或控制台 'file' | 'console'
-    log: 'console'
-}
-
-```
-
-
-#### 启动
-```
-cd server
-node index.js
-```
-
-推荐用`pm2`在生产环境启动，否则你断开ssh，node进程就终止了
-
-```
-npm install pm2 -g
-pm2 start index.js
-```
-如果你在配置文件中选择`log`类型为`file`, 那么输出的日志文件将在默认为server目录下的`disqus-proxy.log`
-
-#### Done !
-
-### (可选) 使用 docker
-
-在安装了docker 的机器上，可以很方便得配置后端。
-
-只需要把 port,your_serect,your_short_name替换成自己的secret 和short_name。
-
-其中port是外部访问的端口。
-
-````shell
-docker run -d --name disqus-proxy -p 5509:port \
--e DISQUS_API_SECRECT=your_serect \
--e DISQUS_SHORT_NAME=your_short_name \
-ycwalker/hexo-disqus-proxy:1.0.6 
-````
-
-### 其他
-
-如果需要https访问，我们可以用nginx来反向代理disqus proxy.
-
-```nginx
-server {
-    listen 443 ssl;
-    server_name disqus.domain.com;
-    ssl_certificate /etc/ssl/startssl/1_disqus.domain.com_bundle.crt;
-    ssl_certificate_key /etc/ssl/startssl/2_disqus.domain.com.key;
-    
-    location / {
-        proxy_set_header  X-Real-IP  $remote_addr;
-        proxy_pass http://host:port$request_uri;
-    }
-}
-```
+**`1.3.0`版本之后将后端分离出来，查看[这里](https://github.com/ciqulover/disqus-proxy-server)进行配置** 
--- index.js
+++ index.js
@@ -1,70 +1,10 @@
 'use strict'
-const fs = require('hexo-fs')
 
-hexo.extend.filter.register('after_render:html', function (str) {
+// diqus原生脚本替换
+hexo.extend.filter.register('after_render:html', require('./lib/after_render'))
 
-  const idReg = /<div +id *= *["']disqus_thread["'] *>/
-  const srcReg = /s.src *= *['"]https:\/\/.+\.disqus\.com\/embed\.js['"];?/
-  const insertReg = /\(d\.head *\|\| *d\.body\)\.appendChild\(s\);?/
+// 引入资源
+hexo.extend.generator.register('assets', require('./lib/assets'))
 
-  if (idReg.test(str)) {
-
-    //CND和外部资源
-    const cdn = `
-      <script src="//cdn.bootcss.com/react/16.0.0/umd/react.production.min.js"></script>
-      <script src="//cdn.bootcss.com/react-dom/16.0.0/umd/react-dom.production.min.js"></script>
-      <script src="//cdn.bootcss.com/babel-polyfill/7.0.0-beta.2/polyfill.min.js"></script>
-      <script src="//cdn.bootcss.com/fetch/2.0.3/fetch.min.js"></script>
-      <script src="//cdn.jsdelivr.net/npm/blockies-identicon@0.1.0/blockies.min.js"></script>
-      <link rel="stylesheet" href="//cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-      `
-    // hexo-disqus-proxy-primary主脚本
-    const script = `<script src="/scripts/hexo-disqus-proxy-primary.js" async></script>`
-
-    // 将disqus_thread转化为disqus_proxy_thread
-    str = str.replace(idReg, cdn + '<div id="disqus_proxy_thread">' + script)
-
-    // 删除原有的disqus评论脚本
-    str = str.replace(srcReg, '')
-    str = str.replace(insertReg, '')
-  }
-
-  return str
-})
-
-hexo.extend.generator.register('assets', function (locals) {
-  return [
-    {
-      path: 'scripts/hexo-disqus-proxy-primary.js',
-      data: function () {
-        return fs.createReadStream('node_modules/hexo-disqus-proxy/lib/hexo-disqus-proxy-primary.js')
-      }
-    }
-  ]
-})
-
-hexo.extend.filter.register('template_locals', function (locals) {
-
-  if (!locals.page) return locals
-
-  const config = hexo.config.disqus_proxy
-
-  const script = `
-      <script>
-        window.disqusProxy={
-          shortname: '${config.shortname}',
-          username: '${config.username}',
-          server: '${config.host}',
-          port: ${config.port},
-          adminAvatar: '${config.admin_avatar ? config.admin_avatar : "/avatars/admin-avatar.jpg"}',
-          identifier: '${locals.page.path}',
-        };
-        window.disqus_config = function () {
-          this.page.url = window.location.href;
-          this.page.identifier = window.disqusProxy.identifier;
-        };
-      </script>`
-  locals.page.content = locals.page.content + script
-
-  return locals
-})
+// 生成配置
+hexo.extend.filter.register('template_locals', require('./lib/template_locals')(hexo))
